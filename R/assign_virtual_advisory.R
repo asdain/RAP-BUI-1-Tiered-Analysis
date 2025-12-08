3262790462f3d1bb2df7421ceac8519daf9fc4ff
asdainqu <16asd@queensu.ca>
2025-12-08 10:30:23 -0700

    Updated logic for dioxins/furans among other data

--- R/assign_virtual_advisory.R
+++ R/assign_virtual_advisory.R
@@ -6,50 +6,67 @@
 #'
 #' @return Integer vector of advisory levels (number of meals/month)
 #' @export
-assign_advisory <- function(conc, population = "General", contaminant = "MERCURY") {
+library(dplyr)
+
+assign_advisory <- function(conc, population = "General", contaminant = c("MERCURY", "PCBs", "TEQWH4")) {
+  contaminant <- match.arg(contaminant)
+  
   if (!population %in% c("General", "Sensitive")) {
     stop("population must be 'General' or 'Sensitive'")
-  }
-  
-  if (!contaminant %in% c("MERCURY", "PCBs")) {
-    stop("Only 'MERCURY' (must be all-caps) and 'PCBs' are currently supported.")
   }
   
   if (contaminant == "MERCURY") {
     if (population == "Sensitive") {
       return(case_when(
-        conc > 0.50  ~ 0L,
-        conc > 0.25  ~ 4L,
-        conc > 0.16  ~ 8L,
-        conc > 0.12  ~ 12L,
-        conc > 0.06  ~ 16L,
-        TRUE         ~ 32L
+        conc > 0.50 ~ 0L,
+        conc > 0.25 ~ 4L,
+        conc > 0.16 ~ 8L,
+        conc > 0.12 ~ 12L,
+        conc > 0.06 ~ 16L,
+        TRUE        ~ 32L
       ))
-    } else {
+    } else {  # General
       return(case_when(
-        conc > 1.8   ~ 0L,
-        conc > 1.2   ~ 2L,
-        conc > 0.6   ~ 4L,
-        conc > 0.4   ~ 8L,
+        conc > 1.8  ~ 0L,
+        conc > 1.2  ~ 2L,
+        conc > 0.6  ~ 4L,
+        conc > 0.4  ~ 8L,
         conc > 0.3  ~ 12L,
-        conc > 0.15   ~ 16L,
-        TRUE         ~ 32L
+        conc > 0.15 ~ 16L,
+        TRUE        ~ 32L
       ))
     }
   }
   
   if (contaminant == "PCBs") {
     return(case_when(
-      conc > 844   ~ 0L,
-      conc > 422   ~ 1L,
-      conc > 211   ~ 2L,
-      conc > 105   ~ 4L,
-      conc > 70    ~ 8L,
-      conc > 53    ~ 12L,
-      conc > 26    ~ 16L,
-      TRUE         ~ 32L
+      conc > 844 ~ 0L,
+      conc > 422 ~ 1L,
+      conc > 211 ~ 2L,
+      conc > 105 ~ 4L,
+      conc > 70  ~ 8L,
+      conc > 53  ~ 12L,
+      conc > 26  ~ 16L,
+      TRUE       ~ 32L
     ))
   }
+  
+  if (contaminant == "TEQWH4") {
+    # Dioxin/Furan/dlPCB TEQ (pg/g)
+    return(case_when(
+      conc > 21.6 ~ 0L,
+      conc > 10.8 ~ 1L,
+      conc > 5.4  ~ 2L,
+      conc > 2.7  ~ 4L,
+      conc > 1.8  ~ 8L,
+      conc > 1.3  ~ 12L,
+      conc > 0.7  ~ 16L,
+      TRUE        ~ 32L
+    ))
+  }
+
+
+  
   
   #  Fallback to prevent NULL return if no match above (safety net)
   stop("Unrecognized input combination. Check contaminant and population.")
